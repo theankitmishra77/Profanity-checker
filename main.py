2d45240a6005ee74b62c32c56d816ff93e58ff7d
Ankit Mishra <73629134+theankitmishra77@users.noreply.github.com>
2025-01-15 18:19:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,4 +8,4 @@ PORT = int(os.get('PORT', 8000))
 HOST = '0.0.0.0'
 
 if __name__ == '__main__':
-    uvicorn.run('app.api:app', host = HOST, port = PORT, reload = True)
+    uvicorn.run('api:app', host = HOST, port = PORT, reload = True)
